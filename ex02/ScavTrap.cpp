fa3cbd4c2fda5974c5ec2f2fbf53d80d0ab3ec23
Mahabub Ahmed <mahabub.ahmed.eee@gmail.com>
2025-07-15 12:42:24 +0200

    file added from previous exercises

---
+++ ex02/ScavTrap.cpp
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gahmed <gahmed@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/14 13:37:42 by gahmed            #+#    #+#             */
+/*   Updated: 2025/07/14 13:37:42 by gahmed           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ScavTrap.hpp"
+
+//default constructor
+ScavTrap::ScavTrap() : ClapTrap("Default_Scav")
+{
+    this->hitPoint = 100;
+    this->energyPoint = 50;
+    this->attackDamage = 20;
+    std::cout << "Scavtrap default constructor called for " << name << "\n";
+}
+
+// parameterize constructor
+ScavTrap::ScavTrap(const std::string& name) : ClapTrap(name)
+{
+    this->hitPoint = 100;
+    this->energyPoint = 50;
+    this->attackDamage = 20;
+    std::cout << "ScavTrap constructor called for " << name << "\n";
+}
+
+// copy constructor
+ScavTrap::ScavTrap(const ScavTrap& obj) : ClapTrap(obj)
+{
+    std::cout << "ScavTrap copy constructor called for " << name << "\n";
+}
+
+// assignment operator
+ScavTrap& ScavTrap::operator=(const ScavTrap& obj)
+{
+    ClapTrap::operator=(obj);
+    std::cout << "ScavTrap copy assignment operator called for " << name << "\n";
+    return *this;
+}
+
+// destructor
+ScavTrap::~ScavTrap()
+{
+    std::cout << "ScavTrap destructor called for " << name << "\n";
+}
+
+// oveerride claptrap's attack
+void ScavTrap::attack(const std::string& target)
+{
+    if (hitPoint <= 0 || energyPoint <= 0)
+    {
+        std::cout << "ScavTrap " << name << " cannot attack. No health or energy!\n";
+        return;
+    }
+    energyPoint--;
+    std::cout << "ScavTrap " << name << " attacks" << target << ", causing "
+                << attackDamage << " damage!\n";
+} 
+
+void ScavTrap::guardGate()
+{
+    std::cout << "ScavTrap " << name << " has entered the Gate keeper mode.\n";
+}
---
+++ ex02/main.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gahmed <gahmed@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/14 15:06:11 by gahmed            #+#    #+#             */
+/*   Updated: 2025/07/14 15:06:11 by gahmed           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ScavTrap.hpp"
+
+int main( void )
+{
+    std::cout << "\n--- Creating ScavTrap 'SCAVVY' ---" << std::endl;
+    ScavTrap scav("SCAVVY");
+    std::cout << "\n--- Stats Check ---" << std::endl;
+    std::cout << "HP: " << scav.getHit()
+              << ", EP: " << scav.getEnergy()
+              << ", DMG: " << scav.getDamage() << std::endl;
+    std::cout << "\n--- ScavTrap attacks 'ENEMY' ---" << std::endl;
+    scav.attack("ENEMY");
+    std::cout << "\n--- ScavTrap takes damage ---" << std::endl;
+    scav.takeDamage(15);
+    std::cout << "\n--- ScavTrap repairs ---" << std::endl;
+    scav.beRepaired(10);
+    std::cout << "\n--- ScavTrap activates Gate Keeper mode ---" << std::endl;
+    scav.guardGate();
+    std::cout << "\n--- End of ScavTrap Test ---" << std::endl;
+    return (0);
+}
